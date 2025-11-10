1b210adc38317794cbaf6656fc5cf68230bd09cb
BennoWu <benno.wu@gmail.com>
2025-11-11 01:17:35 +0800

    更新檔案

--- app.py
+++ app.py
@@ -268,9 +268,10 @@ def handle_message(event):
 
 	# 歲次干支 - 輸入年份 - 輸入干支取得
 	## 兩位數(民國) ，四位數(西元) 可通過
-	elif (  bool(re.fullmatch(r'\d{2}|\d{4}', returnMsg)) ) or ( inputMsg in ganZhi_Dict.values() ):
+	elif (  bool(re.fullmatch(r'\d{2}|\d{4}',  str(returnMsg).strip())) ) or ( inputMsg in ganZhi_Dict.values() ):
 	# elif ( inputMsg.isdigit() == True ) or ( inputMsg in ganZhi_Dict.values() ):		
 	# elif inputMsg == "西元年","民國年","年干支":
+		print ( "干支模式")
 		ui_cmd_dict = getFlexMessage_GZ ( checkYear ( yearData = inputMsg ) )
 		# ⭐ v3 的 Flex Message
 		line_bot_api.reply_message(
