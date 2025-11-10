3fa41e0dea3f0c0120b0079fce0c44ee959d2b88
BennoWu <benno.wu@gmail.com>
2025-11-10 15:20:58 +0800

    更新檔案

--- app.py
+++ app.py
@@ -391,17 +391,17 @@ def handle_message(event):
 
 
 
-		# Notion 處理
-		elif inputMsg.startswith("n+"):
-			notion_url = sixYaoMain(inputMsg, userSetting=userData)
+	# Notion 處理
+	elif inputMsg.startswith("n+"):
+		notion_url = sixYaoMain(inputMsg, userSetting=userData)
 
-			# ⭐ v3 文字訊息回覆
-			line_bot_api.reply_message(
-				ReplyMessageRequest(
-					reply_token=event.reply_token,
-					messages=[TextMessage(text=notion_url)]
-				)
+		# ⭐ v3 文字訊息回覆
+		line_bot_api.reply_message(
+			ReplyMessageRequest(
+				reply_token=event.reply_token,
+				messages=[TextMessage(text=notion_url)]
 			)
+		)
 
 
 
