edb46562372e9539e389d690f472deeca6cd60d5
BennoWu <benno.wu@gmail.com>
2025-12-07 15:21:58 +0800

    更新檔案

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from flexLayout_tool import ganZiList_fun , yearListFlexLayout , getFlexMessage_
 
 from fourPillar_tool import checkYear,getNowTime
 from fourPillar_tool import ganZhi_Dict ## 六十甲子的字典
-from small_six_zan_work import sSixZmain ## 小六壬
+
+from flexLayout_tool import sSixZnUi ## 小六壬
 
 
 
